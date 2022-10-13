2bb1a15145c34e1828d492a4d0d2eb664f6d429f
Jin <512364579@qq.com>
2022-10-13 22:11:16 +0800

    feat: 新增对整体项目的思考

--- packages/ammunition-sunflow/src/runtime/index.ts
+++ packages/ammunition-sunflow/src/runtime/index.ts
@@ -11,6 +11,19 @@ console.log('memoryHistory>>', memoryHistory);
 
 console.log('browerHistory>>', browerHistory);
 
+/**
+ * 1. 使用 browerHistory 或者 hashHistory 做路由跳转，并监听
+ * 2. 监听到路由的变化时需要做的事情？
+ *  - 1. 需要根据当前路由查找到传入路由配置的信息
+ *  - 2. 对前一个路由进行卸载操作
+ *  - 3. 对当前匹配到的路由进行装载操作
+ * 
+ * 3. 需要搞明白 iframe + web component 如何处理内容加载 js隔离 css 隔离
+ *  - 1. 重写 sunflower
+ * 
+ * 4. 如何给 iframe 注入当前插件的能力
+*/
+
 type TypeRunTime = {
   app: any;
   routerConfigs: IRouterConfigs
